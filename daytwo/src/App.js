7c2711e8cc912b9827397a718adf3148fe8974fc
Sailesh Gurung <143858404+grgsailesh85@users.noreply.github.com>
2024-06-02 04:08:13 +0545

    Day_Two

--- daytwo/src/App.js
+++ daytwo/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
 function App() {
-  var x = 10
-  var y = 12
-  var z = x + y
+  var x = 10;
+  var y = 12;
+  var z = x + y;
 
   return (
     <div>
-      <h1>
+      <h1 style = {{
+        color:'red',
+        fontSize: '80px',
+        backgroundColor: 'blue'
+      }}>
         About Me
       </h1>
-      <p>Hello, Everyone!, My name is Sailesh Gurung and I am a fresh graduate Computer Engineer.</p>
+      <p style={{
+        color: 'blue',
+        fontSize:'40px',
+        background: 'red'
+      }}>Hello, Everyone!, My name is Sailesh Gurung and I am a fresh graduate Computer Engineer.</p>
       <h3>Example of jsx</h3>
       <p>The sum of x=10 and y=12 is {z}. </p>
     </div>
