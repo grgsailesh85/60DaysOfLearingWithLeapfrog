0b0da82350d7c3f37b98918b2839252962c572db
Sailesh Gurung <143858404+grgsailesh85@users.noreply.github.com>
2024-06-02 00:56:28 +0545

    Day_Two

---
+++ daytwo/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  var x = 10
+  var y = 12
+  var z = x + y
+
+  return (
+    <div>
+      <h1>
+        About Me
+      </h1>
+      <p>Hello, Everyone!, My name is Sailesh Gurung and I am a fresh graduate Computer Engineer.</p>
+      <h3>Example of jsx</h3>
+      <p>The sum of x=10 and y=12 is {z}. </p>
+    </div>
+  );
+}
+
+export default App;
