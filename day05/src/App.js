918dd7f1d0ceee6d56341e5bdb6b08b0f3d0ba56
Sailesh Gurung <143858404+grgsailesh85@users.noreply.github.com>
2024-06-05 23:08:05 +0545

    Day_Five

---
+++ day05/src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+const App = () =>{
+  return(
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+export default App;
