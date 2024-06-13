4d5bbf7024afea57782f0ab446ff8c36966e8950
Sailesh Gurung <143858404+grgsailesh85@users.noreply.github.com>
2024-06-13 11:29:22 +0545

    Update App.js

--- project03/src/App.js
+++ project03/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+const App = () =>{
+  const [value, setValue] = useState("")
+  const [data, setData] = useState([])
+ 
+  const addList = ()=>{
+    if(value.length === 0){
+      alert("Input Field Is Required")
+    } else {
+      setData([
+        ...data, //old values
+        value    //new values
+      ])
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <h1>Dynamic List (using UseState & Array)</h1>
+      <div>
+        <input
+          onChange={(e)=>setValue(e.target.value)}
+          placeholder='Enter Product Name'
+          style={{
+            border:'1px solid  #ccc',
+            padding: 12, 
+            borderRadius: 4,
+            width:350,            
+          }}/>
+          <button
+            onClick={addList} 
+            style={{
+              border:'none',
+              background:'darkorange',
+              padding:13,
+              fontWeight:'bold',
+              marginLeft:6,
+              width:110,
+              borderRadius:6
+          }}>
+            Add
+          </button>
+      </div>
+
+      <ul>
+        {
+          data.map((item, index)=>(
+            <li key={index}>{item}</li>
+          ))
+        }
+      </ul>
     </div>
-  );
+  )
 }
-
 export default App;
