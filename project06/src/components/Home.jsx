db6fc9e72c2a76fd6ca2ba7ffcac06a843e0d9ef
Sailesh Gurung <143858404+grgsailesh85@users.noreply.github.com>
2024-06-27 19:58:29 +0545

    Update Home.jsx

--- project06/src/components/Home.jsx
+++ project06/src/components/Home.jsx
@@ -3,8 +3,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
-
 import { Navigation, Pagination } from 'swiper/modules';
 const Home = () =>{
     return(
